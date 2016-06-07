2a15b262326724ccb4eb194f714d4d91a47c97aa
ks2355244 <ks2355244@yahoo.com>
2016-06-07 11:49:25 +0800

    trying to solve

--- dictionary_sort.rb
+++ dictionary_sort.rb
@@ -1,5 +1,18 @@
 def dictionary_sort(arr)
   # Your code here to sort the array
-end
+  puts 'type a word:'
+  answer = gets.chomp
 
+  	if 
+  		answer = word 
+  		puts 'type another word'
+
+	else
+		puts "Congratulations! Your dictionary has 4 words:"
+	end
+
+	arr = word.sort{|word|}
+end
+arr = ["word"]
+dictionary_sort(arr)
 # ...your code here to initialize the program and take user input
